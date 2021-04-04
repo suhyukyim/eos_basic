2f82e32fef30695439f473db739c38c5da3d7f72
suhyukyim <81335691+suhyukyim@users.noreply.github.com>
2021-04-04 15:26:38 +0900

    Add files via upload

--- Main2_2.java
+++ Main2_2.java
@@ -3,17 +3,19 @@ package week2;
 public class Main2_2 {
     public static void main(String[] args) {
         int[] min = {5, 10, 12, 35, 60, 100, 300};
+        int sum;
+        sum=0;
         for (int num = 0; num < 7; num++)
             if (min[num] < 10) {
-                System.out.println("주차 요금 합계는 총 0원입니다.");
+                sum=sum+0;
             } else if (10 <= min[num] && min[num] < 30) {
-                System.out.println("주차 요금 합계는 총 500원입니다.");
+                sum=sum+500;
             } else
                 if ((500 + ((min[num] - 20) / 10 * 508)) >= 10000) {
-                System.out.println("주차 요금 합계는 총 10000원입니다");
+                sum=sum+10000;
                 } else {
-                System.out.println("주차 요금 합게는 총 " + (500 + ((min[num] - 20) / 10 * 500))+"입니다.");
+                sum=sum+((500 + ((min[num] - 20) / 10 * 500)));
             }
-
+        System.out.println("주차 요금 합계는 총"+sum+"원입니다.");
     }
 }

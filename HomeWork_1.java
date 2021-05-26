f7080cf4df4ecae85db4d2c0f79e7dbe330e1cc6
suhyukyim <81335691+suhyukyim@users.noreply.github.com>
2021-05-26 11:23:29 +0900

    Eos_Week_7

---
+++ HomeWork_1.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class IndexOutOfBoundsException extends Exception{
+    public IndexOutOfBoundsException(){
+        super("에러발생");
+    }
+}
+public class HomeWork_1 {
+    static void call(int[] str,int num){
+            System.out.println("call value : "+str[num]);
+    }
+    public static void main(String[] args) throws IndexOutOfBoundsException {
+        int[] str = {2,0,0,1,0,7,2,7};
+
+        try {
+            int num = readNum();
+            call(str,num);
+        } finally{
+            System.out.println("해당 문장은 무조건 수행");
+        }
+    }
+    public static int readNum() throws IndexOutOfBoundsException{
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        if(num<0 || num >=8)
+            throw new IndexOutOfBoundsException();
+        return num;
+    }
+}
